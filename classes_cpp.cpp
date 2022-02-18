ab65e901b931c81e4391e644cd4d4c246fe7e02b
N0R4H <rohanpan27@gmail.com>
2022-02-19 00:05:21 +0530

    added classes

--- classes_cpp.cpp
+++ classes_cpp.cpp
@@ -7,27 +7,29 @@
 using namespace std;
 
 
-class ABC{
-  public:
-      int number;
-      string str;
 
-}
+class ABC{          // class name
+  public:           // access identifier
+      int number;   // integer attribute
+      string str;   // string attribute
+
+  ABC(){
+    number = 1;
+    str = "constructor"
+  }
+
+};
 
 
 int main(){
-  ABC obj1;
+  ABC obj1;       //creating object (obj1) of type class ABC
 
-  obj1.number = 45;
-  obj1.str = "type ABC obj1"
+  obj1.number = 45;           //setting integer attribute  of obj1
+  obj1.str = "type ABC obj1"; //setting string  attribute  of obj1
 
-  ABC obj2;
+  ABC obj2;     //creating object (obj2) of type class ABC
 
-  obj2.number = 24;
-  obj2.str = "type ABC obj2"
-
-  cout<<obj1.number + obj2.number<<endl;
-
-  	
-
+  obj2.number = 24;           //setting integer attribute  of obj1
+  obj2.str = "type ABC obj2"; //setting string  attribute  of obj1
+  cout<<obj1.number+obj2.number<<endl;
 }
