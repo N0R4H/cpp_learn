2b5aff3af904d2eabdb75ede2d030362e764389e
N0R4H <rohanpan27@gmail.com>
2022-02-19 13:45:19 +0530

    classes update

--- classes_cpp.cpp
+++ classes_cpp.cpp
@@ -13,6 +13,7 @@ class ABC{          // class name
       int number;   // integer attribute
       string str;   // string attribute
 
+
   //default constructor
   ABC(){
     number = 1;
@@ -25,27 +26,43 @@ class ABC{          // class name
     str = p2;
   };
 
+  //parameterized constructor (same name but different parameter type)
+  ABC(float p1, float p2){
+    number = p1;
+    str = p2;
+  };
 
 
 };
-
 
 int main(){
 
 
   ABC obj1;                 //creating object (obj1) of type class ABC
+  ABC obj2;  
 
   cout<<obj1.str<<" "<<obj1.number<<endl;   //default attribute
-  
-  obj1 = ABC(2,"parameterized constructor");  //creating object (obj1) of 
-                                             //type class ABC with custom attributes
+
+  //=============================OVERLOADING==========================================
+  // OVERLOADING is a feature of OOP by which we can create one or more objects/functions.
+  // C++ tries to find the EXACT function/constructor name with EXACT parameter types
+  // Hence we can have 2 different constructors/functions with same name but different parameter types
+
+  obj1 = ABC(2,97);       //creating object (obj1) of 
+                          //type class ABC  using constructor of EXACT attribute type.
 
   cout<<obj1.str<<" "<<obj1.number<<endl;   //changing default to custom attributes 
+
+  obj2 = ABC(3,"parameterized constructor");//creating object (obj1) of 
+                          //type class ABC  using constructor of EXACT attribute type.
+
+  cout<<obj2.str<<" "<<obj2.number<<endl;  //changing default to custom attributes 
+  //===================================================================================
 
   obj1.number = 45;           //setting integer attribute  of obj1
   obj1.str = "type ABC obj1"; //setting string  attribute  of obj1
 
-  ABC obj2;                   //creating object (obj2) of type class ABC
+                   //creating object (obj2) of type class ABC
 
   obj2.number = 24;           //setting integer attribute  of obj1
   obj2.str = "type ABC obj2"; //setting string  attribute  of obj1
